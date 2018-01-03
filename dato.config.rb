ea111bb81f1a6bd50d95d8ab6cc4ed1216b629cb
Andrew MacMurray <a.macmurray@icloud.com>
2018-01-03 16:38:21 +0000

    adds first three sections to main page

--- dato.config.rb
+++ dato.config.rb
@@ -14,7 +14,7 @@ create_data_file(
 
 taglines = dato.taglines.map do |item|
   {
-    id: item.tagline_id,
+    id: item.tagline_id.parameterize,
     content: item.content
   }
 end
@@ -23,4 +23,18 @@ create_data_file(
   "_data/taglines.yml",
   :yaml,
   taglines
+)
+
+what_we_do = dato.what_we_dos.map do |item|
+  {
+    order: item.order,
+    content: item.content,
+    color: item.color
+  }
+end
+
+create_data_file(
+  "_data/what_we_do.yml",
+  :yaml,
+  what_we_do
 )
