89b7c90ecb3c69e5ef62fad0e29064face9928e9
Andrew MacMurray <a.macmurray@icloud.com>
2018-01-08 17:17:35 +0000

    adds all content for homepage

--- dato.config.rb
+++ dato.config.rb
@@ -1,9 +1,11 @@
 data_files = [
-  "_data/nav_titles.yml",
-  "_data/taglines.yml",
-  "_data/what_we_do.yml",
-  "_data/six_tenets.yml"
-]
+  "nav_titles",
+  "taglines",
+  "what_we_do",
+  "six_tenets",
+  "supporters_and_funders",
+  "social_channels"
+].map { |file| "_data/#{file}.yml" }
 
 data = [
   dato.nav_titles.map{ |item| {
@@ -18,7 +20,21 @@ data = [
     content: item.content,
     color: item.color }
   },
-  dato.six_tenets_2s.map { |item| item.content }
+  dato.tenets.map { |item| item.content },
+  dato.supporter_funders.map { |item| {
+    image_url: "https://www.datocms-assets.com" + item.image.path,
+    image_alt: item.image.alt,
+    image_title: item.image.title,
+    name: item.name,
+    link: item.link }
+  },
+  dato.social_channels.map { |item| {
+    image_url: "https://www.datocms-assets.com" + item.image.path,
+    image_alt: item.image.alt,
+    image_title: item.image.title,
+    link: item.link,
+    name: item.name }
+  }
 ]
 
 data_files.zip(data).map do |xs|
