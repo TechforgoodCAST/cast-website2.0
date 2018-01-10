72a74c824fdd80ebeaed591376a36cb1d693f3a5
Andrew MacMurray <a.macmurray@icloud.com>
2018-01-10 13:08:08 +0000

    adds for charities page

--- dato.config.rb
+++ dato.config.rb
@@ -1,46 +1,72 @@
-data_files = [
+module YamlHelper
+  def YamlHelper.add_origin(path)
+    "https://www.datocms-assets.com" + path
+  end
+
+  def YamlHelper.file_paths(paths)
+    paths.map { |p| YamlHelper.to_file_path(p) }
+  end
+
+  private
+  def YamlHelper.to_file_path(file_name)
+    "_data/#{file_name}.yml"
+  end
+end
+
+paths = YamlHelper.file_paths([
   "nav_titles",
   "taglines",
-  "what_we_do",
-  "six_tenets",
   "supporters_and_funders",
-  "social_channels"
-].map { |file| "_data/#{file}.yml" }
+  "social_channels",
+  "logos",
+  "bespoke_project_orgs"
+])
 
 data = [
   dato.nav_titles.map{ |item| {
     title: item.title,
     url: item.title.parameterize }
   },
-  dato.taglines.map { |item| {
-    id: item.tagline_id.parameterize,
-    content: item.content }
+  {
+    main_headline: dato.tagline.main_headline,
+    what_we_believe: dato.tagline.what_we_believe,
+    we_work_with_funders: dato.tagline.we_work_with_funders,
+    how_we_help_charities: dato.tagline.how_we_help_charities,
+    fuse_blurb: dato.tagline.fuse_blurb,
+    fellowship_blurb: dato.tagline.fellowship_blurb,
+    training_and_learning: dato.tagline.training_and_learning,
+    know_your_audience: dato.tagline.know_your_audience,
+    prototyping: dato.tagline.prototyping,
+    user_testing: dato.tagline.user_testing,
+    sustainability: dato.tagline.sustainability
   },
-  dato.what_we_dos.map { |item| {
-    content: item.content,
-    color: item.color }
-  },
-  dato.tenets.map { |item| item.content },
   dato.supporter_funders.map { |item| {
-    image_url: "https://www.datocms-assets.com" + item.image.path,
+    image_url: YamlHelper.add_origin(item.image.path),
     image_alt: item.image.alt,
     image_title: item.image.title,
     name: item.name,
     link: item.link }
   },
   dato.social_channels.map { |item| {
-    image_url: "https://www.datocms-assets.com" + item.image.path,
+    image_url: YamlHelper.add_origin(item.image.path),
     image_alt: item.image.alt,
     image_title: item.image.title,
     link: item.link,
     name: item.name }
+  },
+  {
+    fuse: YamlHelper.add_origin(dato.logo.fuse_logo.path),
+    fellowship: YamlHelper.add_origin(dato.logo.fellowship_logo.path)
+  },
+  dato.bespoke_project_orgs.map { |item| {
+    image_url: YamlHelper.add_origin(item.image.path),
+    image_alt: item.image.alt,
+    image_title: item.image.title,
+    name: item.name,
+    link: item.link }
   }
 ]
 
-data_files.zip(data).map do |xs|
-  create_data_file(
-    xs[0],
-    :yaml,
-    xs[1]
-  )
+paths.zip(data).map do |xs|
+  create_data_file(xs[0], :yaml, xs[1])
 end
