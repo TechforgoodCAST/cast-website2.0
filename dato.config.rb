8a818cd5b5fc392b1abe2ae70fd1157299ad8796
Andrew MacMurray <a.macmurray@icloud.com>
2018-01-15 13:07:11 +0000

    use hashmap for each dato collection

--- dato.config.rb
+++ dato.config.rb
@@ -3,76 +3,85 @@ module YamlHelper
     "https://www.datocms-assets.com" + path
   end
 
-  def YamlHelper.file_paths(paths)
-    paths.map { |p| YamlHelper.to_file_path(p) }
-  end
-
-  private
   def YamlHelper.to_file_path(file_name)
     "_data/#{file_name}.yml"
   end
 end
 
-paths = YamlHelper.file_paths([
-  "supporters_and_funders",
-  "clients_and_supporters",
-  "social_channels",
-  "bespoke_project_orgs",
-  "fuse_alumni",
-  "team",
-  "board"
-])
-
+# file name and corresponding data to be fetched from dato
 data = [
-  dato.supporter_funders.map { |item| {
-    image_url: YamlHelper.dato_origin(item.image.path),
-    image_alt: item.image.alt,
-    image_title: item.image.title,
-    name: item.name,
-    link: item.link }
+  {
+    file_name: "supporters_and_funders",
+    data: dato.supporter_funders.map { |item| {
+      image_url: YamlHelper.dato_origin(item.image.path),
+      image_alt: item.image.alt,
+      image_title: item.image.title,
+      name: item.name,
+      link: item.link }
+    }
   },
-  dato.client_supporters.map { |item| {
-    image_url: YamlHelper.dato_origin(item.image.path),
-    image_alt: item.image.alt,
-    image_title: item.image.title,
-    name: item.name,
-    link: item.link }
+  {
+    file_name: "clients_and_supporters",
+    data: dato.client_supporters.map { |item| {
+      image_url: YamlHelper.dato_origin(item.image.path),
+      image_alt: item.image.alt,
+      image_title: item.image.title,
+      name: item.name,
+      link: item.link }
+    }
   },
-  dato.social_channels.map { |item| {
-    image_url: YamlHelper.dato_origin(item.image.path),
-    image_alt: item.image.alt,
-    image_title: item.image.title,
-    link: item.link,
-    name: item.name,
-    description: item.description }
+  {
+    file_name: "social_channels",
+    data: dato.social_channels.map { |item| {
+      image_url: YamlHelper.dato_origin(item.image.path),
+      image_alt: item.image.alt,
+      image_title: item.image.title,
+      link: item.link,
+      name: item.name,
+      description: item.description }
+    }
   },
-  dato.bespoke_project_orgs.map { |item| {
-    image_url: YamlHelper.dato_origin(item.image.path),
-    image_alt: item.image.alt,
-    image_title: item.image.title,
-    name: item.name,
-    link: item.link }
+  {
+    file_name: "bespoke_project_orgs",
+    data: dato.bespoke_project_orgs.map { |item| {
+      image_url: YamlHelper.dato_origin(item.image.path),
+      image_alt: item.image.alt,
+      image_title: item.image.title,
+      name: item.name,
+      link: item.link }
+    }
   },
-  dato.fuse_alumnis.map { |item| {
-    image_url: YamlHelper.dato_origin(item.image.path),
-    image_alt: item.image.alt,
-    image_title: item.image.title,
-    link: item.link,
-    name: item.name }
+  {
+    file_name: "fuse_alumni",
+    data: dato.fuse_alumnis.map { |item| {
+      image_url: YamlHelper.dato_origin(item.image.path),
+      image_alt: item.image.alt,
+      image_title: item.image.title,
+      link: item.link,
+      name: item.name }
+    }
   },
-  dato.teams.map { |item| {
-    image_url: YamlHelper.dato_origin(item.image.path),
-    name: item.name,
-    job_title: item.job_title,
-    description: item.description }
+  {
+    file_name: "team",
+    data: dato.teams.map { |item| {
+      image_url: YamlHelper.dato_origin(item.image.path),
+      name: item.name,
+      job_title: item.job_title,
+      description: item.description }
+    }
   },
-  dato.boards.map { |item| {
-    image_url: YamlHelper.dato_origin(item.image.path),
-    name: item.name,
-    description: item.description }
+  {
+    file_name: "board",
+    data: dato.boards.map { |item| {
+      image_url: YamlHelper.dato_origin(item.image.path),
+      name: item.name,
+      description: item.description }
+    }
   }
 ]
 
-paths.zip(data).map do |xs|
-  create_data_file(xs[0], :yaml, xs[1])
+# creates yaml files for each collection of data
+data.each do |xs|
+  file = YamlHelper.to_file_path(xs[:file_name])
+  create_data_file(file, :yaml, xs[:data])
 end
