1f2b1f3ed3079342032b8642c1cd272f3e99eafc
Andrew MacMurray <a.macmurray@icloud.com>
2018-01-23 10:37:08 +0000

    adds mailchimp validation and styling to form

---
+++ _js/mailchimp-validate.js
@@ -0,0 +1,1 @@
+(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);
